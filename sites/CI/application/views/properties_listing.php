9ae6715bc7789e9ab32b802d6f3edb3623ac0ce5
Faisal Khan <faisalkhan1222@gmail.com>
2018-08-30 20:57:08 -0400

    Changed the default view route, created a simple view

---
+++ sites/CI/application/views/properties_listing.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>List Of Properties</title>
+</head>
+<body>
+
+<div>
+	Next Step is to read data from DB using the Model and then pass data to the view using an associative array
+</div>
+
+</body>
+</html>
